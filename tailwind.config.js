47a8a5cc3d4342b3e3331caa349c99adaf797b19
thibault-peronno <peronnothibault@hotmail.fr>
2024-05-08 09:45:53 +0200

    config color and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{jsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#679436",
+        second: "#A4BD01",
+        background: "#edf2ca",
+        text:"#2d3114",
+        evidence_1: "#06668C",
+        evidence_2: "#427AA1"
+      }
+    },
   },
   plugins: [],
 }
