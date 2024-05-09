d49984bc16257a4865e5bf31391e171ef262796c
thibault-peronno <peronnothibault@hotmail.fr>
2024-05-09 10:49:41 +0200

    create Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Picture from "../components/Picture";
 import homeImage from "../assets/images/pictures/home.png";
+import Button from "../components/Button";
 
 function Home() {
     return (
         <section className="pt-10">
-            <div>
-                <h1 className="text-5xl font-bold"><span className="text-7xl text-evidence_1">Organise</span> ton voyage.</h1>
-                <Picture src={homeImage} size=" " alt=" "/>
+            <div className="flex items-center flex-col">
+                <h1 className="text-5xl font-bold mb-10"><span className="text-7xl text-evidence_1">Organise</span> ton voyage.</h1>
+                <Picture src={homeImage} size="smallWidth" alt=" "/>
+                <Button text="Commence l'aventure" textColor="textColor" textBg="primary"/>
             </div>
         </section>
     )
