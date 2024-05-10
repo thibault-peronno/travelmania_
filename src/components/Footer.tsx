ecf1acf9c537e1288c2eb3cf2409be3f74a69385
thibault-peronno <peronnothibault@hotmail.fr>
2024-05-10 13:57:46 +0200

    footer V1 done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 function Footer() {
     return(
-        <>
-        Footer component
-        </>
+        <div className="mt-5 p-2.5 border-t-[1px] border-primary" >
+            <div className="text-center">
+                <p className="font-bold text-evidence_1">Thibault PERONNO</p>
+                <p>Test technique avec React et typescript</p>
+            </div>
+
+        </div>
     )
 }
 
