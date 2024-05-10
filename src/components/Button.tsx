b7cb49f52bc0a321ec719cfa8bb38a9271a01121
thibault-peronno <peronnothibault@hotmail.fr>
2024-05-10 13:23:13 +0200

    button component V1 done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,25 +3,11 @@ interface ButtonProps {
     text:string;
     textColor:string;
     textBg:string;
-    sizeButon:string
-    sizeFont:string
 
 }
 function Button(props: Readonly<ButtonProps>) {
-    const sizeButonClass: {[key:string]: string} = {
-        fullWidth: 'w-full',
-        mediumWidth: 'w-56',
-        smallWidth: 'w-26'
-    };
-    const sizeFontButonClass: {[key:string]: string} = {
-        medium: 'text-base',
-        large: 'text-lg',
-        xLarge: 'text-xl',
-        xxLarge: 'text-3xl'
-    };
-    const sizeButton:string = sizeButonClass[props.sizeButon]
-    const sizeFontButton:string = sizeFontButonClass[props.sizeFont]
-    const buttonClass = `bg-${props.textBg} text-${props.textColor} ${sizeButton} ${sizeFontButton} px-3.5 py-2 rounded font-bold hover:scale-105`;
+    
+    const buttonClass = `bg-${props.textBg} text-${props.textColor} w-full text-xl px-8 py-4 rounded font-bold tracking-wider sm:w-max hover:scale-105`;
     return (
         <div className="min-w-full">
             <button className={buttonClass}>{props.text}</button>
