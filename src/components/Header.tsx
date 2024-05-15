f9bb3837d6b8866c81edbbb0b0df63aed255372e
thibault-peronno <peronnothibault@hotmail.fr>
2024-05-15 08:47:27 +0200

    added link on accueil and login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,13 @@ import connectLogo from "../assets/images/icons/connexion.png";
 
 function Header() {
   return (
-      <div className="p-2.5 flex justify-between items-center border-b-[1px] border-primary">
-    <nav>
-       <Link to="#">Accueil</Link>
-    </nav>
-      <Icon src={connectLogo} size="medium" alt="se connecter" />
+    <div className="p-2.5 flex justify-between items-center border-b-[1px] border-primary">
+      <nav>
+        <Link to="/">Accueil</Link>
+      </nav>
+      <Link to="/login">
+        <Icon src={connectLogo} size="medium" alt="se connecter" />
+      </Link>
     </div>
   );
 }
